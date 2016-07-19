69828f4e79bbf31e151156291424913a163ab724
wandalen <wandalen.me@gmail.com>
2016-07-19 15:44:11 +0300

    fix persistent issue

--- staging/abase/syn/Consequence.s
+++ staging/abase/syn/Consequence.s
@@ -877,6 +877,7 @@ var _handleGot = function _handleGot()
 {
   var self = this;
   var result;
+  var spliced = 0;
 
   if( !self._taker.length && !self._takerPersistent.length )
   return;
@@ -913,7 +914,10 @@ var _handleGot = function _handleGot()
   {
 
     if( !_taker.tapping && ordinary )
-    self._given.splice( 0,1 );
+    {
+      spliced = 1;
+      self._given.splice( 0,1 );
+    }
 
     try
     {
@@ -981,6 +985,11 @@ var _handleGot = function _handleGot()
     self._taker.splice( 0,1 );
     _giveTo( _taker,1 );
   }
+
+  /* persistent splice */
+
+  if( !spliced && self._takerPersistent.length )
+  self._given.splice( 0,1 );
 
   /**/
 
