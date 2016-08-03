8501c681ea5ea57c7754b2a9585cfe71c7e59e93
iostash <iostashvxi@gmail.com>
2016-08-03 18:35:23 +0300

    added ReadersWritersProblem.js

---
+++ sample/ReadersWritersProblem.js
@@ -0,0 +1,6 @@
+/*
+ Problem describes situation in which many threads try to access the same shared resource at one time. Some threads may
+ read and some may write, with the constraint that no process may access the share for either reading or writing, while
+ another process is in the act of writing to it. (In particular, it is allowed for two or more readers to access the share
+ at the same time.)
+ */
