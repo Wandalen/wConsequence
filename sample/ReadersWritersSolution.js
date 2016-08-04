e8dd06fb08d0b0ad152d45249f1ee7530eed7f16
iostash <iostashvxi@gmail.com>
2016-08-04 15:13:48 +0300

    read write solution: working draft

---
+++ sample/ReadersWritersSolution.js
@@ -0,0 +1,86 @@
+if( typeof module !== 'undefined' )
+{
+  var _ = require( 'wTools' );
+  /* require( 'wConsequence' ); */
+  require( '../staging/abase/syn/Consequence.s' );
+  var ReadersWritersProblem = require( './ReadersWritersProblem.js' );
+}
+
+var sResource = Object.create( ReadersWritersProblem.resource,
+  {
+    writeLock:
+    {
+      value: wConsequence().give() // on begin resource allowed wor read/write by any subject.
+    },
+
+    readLock:
+    {
+      value: wConsequence().give() // on begin resource allowed wor read by any subject.
+    },
+  }
+);
+
+function SReadersWritersProblem () {};
+
+SReadersWritersProblem.prototype = Object.create( ReadersWritersProblem, {
+  resource:
+  {
+    value: sResource
+  },
+
+  precessEvent :
+  {
+    value: function( opt )
+    {
+      console.log( 'try to perform ' + opt.event.operation + ' by ' + opt.rwSubject.name + ' at '
+        + _.timeSpent( ' ', opt.time ) );
+
+      var resource = this.resource;
+
+      if( opt.event.operation == 'read' )
+      {
+        resource.readLock.got( function()
+          {
+            if( resource.writeLock.hasMessage() !== 0 ) // prevent writing during process read if not prevented by any
+                                                        // other reader yet.
+            {
+              resource.writeLock.got();
+            }
+            resource.readLock.give(); // allow reading, because reading allowed for several readers in one time
+            console.log( 'start read by ' + opt.rwSubject.name );
+            setTimeout( ( function( opt )
+            {
+              console.log( 'end read by ' + opt.rwSubject.name );
+              opt.rwSubject.read();
+              if( resource.writeLock.hasMessage() === 0 )
+              {
+                resource.writeLock.give(); // allow writing
+              }
+            } ).bind( null, opt ), opt.event.duration );
+          }
+        );
+      }
+      else if( opt.event.operation == 'write' )
+      {
+
+        resource.writeLock.got( function()
+          {
+            resource.readLock.got(); // prevent reading during process write
+            console.log( 'start write by ' + opt.rwSubject.name );
+            setTimeout( ( function( opt )
+            {
+              console.log( 'end write by ' + opt.rwSubject.name );
+              opt.rwSubject.write();
+              resource.writeLock.give(); // allow write
+              resource.readLock.give(); //allow read
+            } ).bind( null, opt ), opt.event.duration );
+          }
+        );
+      }
+    }
+  }
+} );
+
+var readersWritersProblem = new SReadersWritersProblem();
+
+readersWritersProblem.init( ReadersWritersProblem.RWSubject );
