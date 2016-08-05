b79e35b14e7a26bc17ec0d9432adfcfeaaaa1451
iostash <iostashvxi@gmail.com>
2016-08-05 10:44:22 +0300

    update read-write solution

--- sample/ReadersWritersSolution.js
+++ sample/ReadersWritersSolution.js
@@ -8,112 +8,97 @@ if( typeof module !== 'undefined' )
 
 //
 
-var sResource = Object.create( ReadersWritersProblem.resource,
-  {
-    writeLock :
-    {
-      value : wConsequence().give() // on begin resource allowed wor read/write by any subject.
-    },
-
-    readLock :
-    {
-      value : wConsequence().give() // on begin resource allowed wor read by any subject.
-    },
-  }
-);
+var writeLock = wConsequence().give(), // on begin resource allowed wor read/write by any subject.
+  readLock = wConsequence().give(); // on begin resource allowed wor read by any subject.
 
 function SReadersWritersProblem () {};
 
 //
+  SReadersWritersProblem.prototype = ReadersWritersProblem;
 
-var properties =
+  SReadersWritersProblem.prototype.constructor = SReadersWritersProblem;
+//
+
+SReadersWritersProblem.prototype.precessEvent = function( opt )
 {
-  resource :
+  console.log( 'try to perform ' + opt.event.operation + ' by ' + opt.rwSubject.name + ' at '
+    + _.timeSpent( ' ', opt.time ) );
+
+  var resource = this.resource;
+
+  if( opt.event.operation == 'read' )
   {
-    value : sResource
-  },
-
-  precessEvent  :
+    performRead( resource, opt );
+  }
+  else if( opt.event.operation == 'write' )
   {
-    value : function( opt )
-    {
-      console.log( 'try to perform ' + opt.event.operation + ' by ' + opt.rwSubject.name + ' at '
-        + _.timeSpent( ' ', opt.time ) );
-
-      var resource = this.resource;
-
-      if( opt.event.operation == 'read' )
-      {
-        resource.readLock.got( function()
-          {
-            if( resource.writeLock.hasMessage() !== 0 ) // prevent writing during process read if not prevented by any
-            // other reader yet.
-            {
-              resource.writeLock.got();
-            }
-            resource.readLock.give(); // allow reading, because reading allowed for several readers in one time
-            if ( resource.writers.length > 0 )
-            {
-              console.log('PROBLEM : ' + opt.rwSubject.name + 'can`t read resource, because it busy by ' +
-                resource.writers.join( ', ' ) + 'subjects' );
-              if( resource.writeLock.hasMessage() === 0 )
-              {
-                resource.writeLock.give(); // allow writing
-              }
-            }
-            else
-            {
-              resource.readers.push( opt.rwSubject.name );
-              console.log( 'start read by ' + opt.rwSubject.name );
-              setTimeout( ( function( opt )
-              {
-                console.log( 'end read by ' + opt.rwSubject.name );
-                opt.rwSubject.read();
-                _.arrayRemovedOnce(resource.readers, opt.rwSubject.name);
-                if( resource.writeLock.hasMessage() === 0 )
-                {
-                  resource.writeLock.give(); // allow writing
-                }
-              } ).bind( null, opt ), opt.event.duration );
-            }
-          }
-        );
-      }
-      else if( opt.event.operation == 'write' )
-      {
-
-        resource.writeLock.got( function()
-          {
-            resource.readLock.got(); // prevent reading during process write
-            if ( resource.writers.length + resource.readers.length > 0 )
-            {
-              console.log('PROBLEM : ' + opt.rwSubject.name + 'can`t wwrite resource, because it busy by ' +
-                resource.readers.join( ', ' ) + ' read subjects and ' + resource.writers.join( ', ' ) + ' write subjects' );
-              resource.writeLock.give(); // allow write
-              resource.readLock.give(); //allow read
-            }
-            else
-            {
-              resource.writers.push( opt.rwSubject.name );
-              console.log( 'start write by ' + opt.rwSubject.name );
-              setTimeout( ( function( opt )
-              {
-                console.log( 'end write by ' + opt.rwSubject.name );
-                opt.rwSubject.write();
-                _.arrayRemovedOnce(resource.writers, opt.rwSubject.name);
-                resource.writeLock.give(); // allow write
-                resource.readLock.give(); //allow read
-              } ).bind( null, opt ), opt.event.duration );
-            }
-          }
-        );
-      }
-    }
+    performWrite( resource, opt );
   }
 }
 
-SReadersWritersProblem.prototype = Object.create( ReadersWritersProblem,properties );
+function performWrite( resource, opt ) {
+  writeLock.got( function()
+  {
+    readLock.got(); // prevent reading during process write
+    if ( resource.writers.length + resource.readers.length > 0 )
+    {
+      console.log('PROBLEM : ' + opt.rwSubject.name + 'can`t wwrite resource, because it busy by ' +
+        resource.readers.join( ', ' ) + ' read subjects and ' + resource.writers.join( ', ' ) + ' write subjects' );
+      writeLock.give(); // allow write
+      readLock.give(); //allow read
+    }
+    else
+    {
+      resource.writers.push( opt.rwSubject.name );
+      console.log( 'start write by ' + opt.rwSubject.name );
+      setTimeout( ( function( opt )
+      {
+        console.log( 'end write by ' + opt.rwSubject.name );
+        opt.rwSubject.write();
+        _.arrayRemovedOnce(resource.writers, opt.rwSubject.name);
+        writeLock.give(); // allow write
+        readLock.give(); //allow read
+      } ).bind( null, opt ), opt.event.duration );
+    }
+  });
+}
+
+function performRead( resource, opt ) {
+  readLock.got( function()
+  {
+    if( writeLock.hasMessage() !== 0 ) // prevent writing during process read if not prevented by any
+    // other reader yet.
+    {
+      writeLock.got();
+    }
+    readLock.give(); // allow reading, because reading allowed for several readers in one time
+    if ( resource.writers.length > 0 )
+    {
+      console.log('PROBLEM : ' + opt.rwSubject.name + 'can`t read resource, because it busy by ' +
+        resource.writers.join( ', ' ) + 'subjects' );
+      if( writeLock.hasMessage() === 0 )
+      {
+        writeLock.give(); // allow writing
+      }
+    }
+    else
+    {
+      resource.readers.push( opt.rwSubject.name );
+      console.log( 'start read by ' + opt.rwSubject.name );
+      setTimeout( ( function( opt )
+      {
+        console.log( 'end read by ' + opt.rwSubject.name );
+        opt.rwSubject.read();
+        _.arrayRemovedOnce(resource.readers, opt.rwSubject.name);
+        if( writeLock.hasMessage() === 0 )
+        {
+          writeLock.give(); // allow writing
+        }
+      } ).bind( null, opt ), opt.event.duration );
+    }
+  });
+}
 
 var readersWritersProblem = new SReadersWritersProblem();
 
-readersWritersProblem.init( ReadersWritersProblem.RWSubject );
+readersWritersProblem.init();
