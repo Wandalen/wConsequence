d6ac207ab41092c5a22433be1b2210c38e415e2e
iostash <iostashvxi@gmail.com>
2016-08-10 12:23:11 +0300

    added h20 solution

---
+++ sample/BuildingH2OSolution.js
@@ -0,0 +1,44 @@
+if( typeof module !== 'undefined' )
+{
+  var _ = require( 'wTools' );
+  /* require( 'wConsequence' ); */
+  require( '../staging/abase/syn/Consequence.s' );
+  var H2OProblem = require( './BuildingH2OProblem.js' );
+}
+
+var waitingH = wConsequence(),
+  molecule = 0;
+
+function oxygen()
+{
+  console.log('oxy');
+  waitingH.got();
+  waitingH.got( this.createWaterMolecule );
+}
+
+function hydrogen()
+{
+  console.log('hidro');
+  waitingH.give();
+}
+
+function createWaterMolecule() {
+  molecule++;
+  console.log( 'water molecule # ' + molecule + ' creating' );
+}
+
+//
+
+function H2OSolution()
+{
+  this.oxygen = oxygen;
+  this.hydrogen = hydrogen;
+  this.createWaterMolecule = createWaterMolecule;
+}
+
+
+
+H2OSolution.prototype = H2OProblem;
+H2OSolution.prototype.constructor = H2OSolution;
+var h2OSolution = new H2OSolution();
+h2OSolution.appendElements();
