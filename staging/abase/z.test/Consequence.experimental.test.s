72d8bd2ab937b377f47f0fc1f0cc0863d32f3bf6
iostash <iostashvxi@gmail.com>
2016-08-19 16:36:42 +0300

    added tests for thenOnce

--- staging/abase/z.test/Consequence.experimental.test.s
+++ staging/abase/z.test/Consequence.experimental.test.s
@@ -48,7 +48,7 @@
           ]
         },
         {
-          givSequence: [ 5, 4  ],
+          givSequence: [ 5, 4 ],
           gotSequence: [],
           expectedSequence:
             [
@@ -228,6 +228,250 @@
     return conseqTester;
   };
 
+  //
+
+  var thenOnce = function( test )
+  {
+
+    var testCase1 =
+
+      {
+        givSequence: [ 5 ],
+        got:
+        {
+          gotSequence: [],
+          throwErr: false
+        },
+        expected:
+        {
+          gotSequence:
+            [
+              { err: null, value: 5, takerId: 'taker1' }
+            ],
+          throwErr: false
+        }
+      },
+      testCase2 =
+      {
+        givSequence:
+          [
+            'err msg'
+          ],
+        got:
+        {
+          gotSequence: [],
+          throwErr: false
+        },
+        expected:
+        {
+          gotSequence:
+            [
+              { err: 'err msg', value: void 0, takerId: 'taker1' }
+            ],
+          throwErr: false
+        }
+      },
+      testCase3 =
+      {
+        givSequence: [ 5, 4 ],
+        got:
+        {
+          gotSequence: [],
+          throwErr: false
+        },
+        expected:
+        {
+          gotSequence:
+            [
+              { err: null, value: 5, takerId: 'taker1' },
+              { err: null, value: 4, takerId: 'taker2' },
+              { err: null, value: 6, takerId: 'taker3' }
+            ],
+          throwErr: false
+        }
+      },
+      testCase4 =
+      {
+        givSequence: [ 5 ],
+        got:
+        {
+          gotSequence: [],
+          throwErr: false
+        },
+        expected:
+        {
+          gotSequence:
+            [
+              { err: null, value: 5, takerId: 'taker1' },
+              { err: null, value: 6, takerId: 'taker2' },
+            ],
+          throwErr: false
+        }
+      };
+
+
+    /* common wConsequence corespondent tests. */
+
+    test.description = 'single value in give sequence, and single taker: attached taker after value resolved';
+    ( function ( { givSequence, got, expected }  )
+    {
+      function testTaker1( err, value )
+      {
+        var takerId = 'taker1';
+        got.gotSequence.push( { err, value, takerId } );
+      }
+
+      var con = wConsequence();
+      con.give( givSequence.shift() );
+      try
+      {
+        con.thenOnce( testTaker1 );
+      }
+      catch( err )
+      {
+        got.throwErr = !! err;
+      }
+      test.identical( got, expected );
+    } )( testCase1 );
+
+    /**/
+
+    test.description = 'single err in give sequence, and single taker: attached taker after value resolved';
+    ( function ( { givSequence, got, expected }  )
+    {
+      function testTaker1( err, value )
+      {
+        var takerId = 'taker1';
+        got.gotSequence.push( { err, value, takerId } );
+      }
+
+      var con = wConsequence();
+      try
+      {
+        con.error( givSequence.shift() );
+        con.thenOnce( testTaker1 );
+      }
+      catch( err )
+      {
+        got.throwErr = !! err;
+      }
+      test.identical( got, expected );
+    } )( testCase2 );
+
+    /**/
+
+    test.description = 'test thenOnce in chain';
+
+    ( function ( { givSequence, got, expected }  )
+    {
+      function testTaker1( err, value )
+      {
+        var takerId = 'taker1';
+        got.gotSequence.push( { err, value, takerId } );
+        value++;
+        return value;
+      }
+
+      function testTaker2( err, value )
+      {
+        var takerId = 'taker2';
+        got.gotSequence.push( { err, value, takerId } );
+      }
+
+      function testTaker3( err, value )
+      {
+        var takerId = 'taker3';
+        got.gotSequence.push( { err, value, takerId } );
+      }
+
+      var con = wConsequence();
+      for (let given of givSequence)
+        con.give( given );
+
+      try
+      {
+        con.thenOnce( testTaker1 );
+        con.thenOnce( testTaker2 );
+        con.got( testTaker3 );
+      }
+      catch( err )
+      {
+        got.throwErr = !! err;
+      }
+      test.identical( got, expected );
+    } )( testCase3 );
+
+    /* test particular thenOnce features test. */
+
+    test.description = 'added several corespondents with same name';
+    ( function ( { givSequence, got, expected }  )
+    {
+      function testTaker1( err, value )
+      {
+        var takerId = 'taker1';
+        got.gotSequence.push( { err, value, takerId } );
+        value++;
+        return value;
+      }
+
+      function testTaker2( err, value )
+      {
+        var takerId = 'taker2';
+        got.gotSequence.push( { err, value, takerId } );
+      }
+
+      function testTaker3( err, value )
+      {
+        var takerId = 'taker3';
+        got.gotSequence.push( { err, value, takerId } );
+      }
+
+      var con = wConsequence();
+
+      try
+      {
+        con.thenOnce( testTaker1 );
+        con.thenOnce( testTaker1 );
+        con.thenOnce( testTaker2 );
+
+        for( let given of givSequence )
+        {
+          con.give( given );
+        }
+      }
+      catch( err )
+      {
+        console.log(err);
+        got.throwErr = !! err;
+      }
+      test.identical( got, expected );
+    } )( testCase4 );
+
+
+    if( Config.debug )
+    {
+      var conDeb1 = wConsequence();
+
+      test.description = 'try to pass as parameter anonymous function';
+      test.shouldThrowError( function()
+      {
+        conDeb1.thenOnce( function( err, val) { logger.log( 'i em anonymous' ); } );
+      } );
+
+      var conDeb2 = wConsequence();
+
+      test.description = 'try to pass as parameter anonymous function (defined in expression)';
+
+      var testHandler = function( err, val) { logger.log( 'i em anonymous' ); }
+      test.shouldThrowError( function()
+      {
+        conDeb2.thenOnce( testHandler );
+      } );
+    }
+
+  };
+
+
   // --
   // proto
   // --
@@ -240,6 +484,7 @@
     {
 
       gotOnce: gotOnce,
+      thenOnce: thenOnce
 
     },
     name : 'Consequence experimental features',
