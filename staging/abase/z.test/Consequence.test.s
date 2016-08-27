bc826994749df4c19e85f3421c8e98a9576c9794
iostash <iostashvxi@gmail.com>
2016-08-27 13:35:17 +0300

    added tests for ifNoErrorThen

--- staging/abase/z.test/Consequence.test.s
+++ staging/abase/z.test/Consequence.test.s
@@ -1038,30 +1038,6 @@ var then = function( test )
             ],
           throwErr: false
         }
-      },
-      testCase4 =
-      {
-        givSequence: [ 5 ],
-        got:
-        {
-          gotSequence: [],
-          throwErr: false
-        },
-        expected:
-        {
-          gotSequence:
-            [
-              {
-                err: null,
-                value: 5,
-                takerId: 'taker1',
-                context: 'ContextConstructor',
-                sealed: 'bar' ,
-                contVariable: 'foo'
-              },
-            ],
-          throwErr: false
-        }
       };
 
 
@@ -1226,30 +1202,6 @@ var then = function( test )
             ],
           throwErr: false
         }
-      },
-      testCase4 =
-      {
-        givSequence: [ 5 ],
-        got:
-        {
-          gotSequence: [],
-          throwErr: false
-        },
-        expected:
-        {
-          gotSequence:
-            [
-              {
-                err: null,
-                value: 5,
-                takerId: 'taker1',
-                context: 'ContextConstructor',
-                sealed: 'bar' ,
-                contVariable: 'foo'
-              },
-            ],
-          throwErr: false
-        }
       };
 
 
@@ -1360,6 +1312,174 @@ var then = function( test )
 
   };
 
+  //
+
+  var ifNoErrorThen = function( test )
+  {
+
+    var testCase1 =
+
+      {
+        givSequence: [ 5 ],
+        got:
+        {
+          gotSequence: [],
+          throwErr: false
+        },
+        expected:
+        {
+          gotSequence:
+          [
+            { err: null, value: 5, takerId: 'taker1' }
+          ],
+          throwErr: false
+        }
+      },
+      testCase2 =
+      {
+        givSequence:
+          [
+            'err msg'
+          ],
+        got:
+        {
+          gotSequence: [],
+          throwErr: false
+        },
+        expected:
+        {
+          gotSequence:
+            [ ],
+          throwErr: false
+        }
+      },
+      testCase3 =
+      {
+        givSequence: [ 5, 'err msg',  4 ],
+        got:
+        {
+          gotSequence: [],
+          throwErr: false
+        },
+        expected:
+        {
+          gotSequence:
+            [
+              { err: null, value: 5, takerId: 'taker1' },
+              { err: 'err msg', value: void 0, takerId: 'taker3' },
+            ],
+          throwErr: false
+        }
+      };
+
+
+    /* common wConsequence corespondent tests. */
+
+    test.description = 'single value in give sequence, and single taker: attached taker after value resolved';
+    ( function ( { givSequence, got, expected }  )
+    {
+      function testTaker1( err, value )
+      {
+        var takerId = 'taker1';
+        got.gotSequence.push( { err, value, takerId } );
+      }
+
+      var con = wConsequence();
+      con.give( givSequence.shift() );
+      try
+      {
+        con.ifNoErrorThen( testTaker1 );
+      }
+      catch( err )
+      {
+        got.throwErr = !! err;
+      }
+      test.identical( got, expected );
+    } )( testCase1 );
+
+    /**/
+
+    test.description = 'single err in give sequence, and single taker: attached taker after value resolved';
+    ( function ( { givSequence, got, expected }  )
+    {
+      function testTaker1( err, value )
+      {
+        var takerId = 'taker1';
+        got.gotSequence.push( { err, value, takerId } );
+      }
+
+      var con = wConsequence();
+      try
+      {
+        con.error( givSequence.shift() );
+        con.ifNoErrorThen( testTaker1 );
+      }
+      catch( err )
+      {
+        got.throwErr = !! err;
+      }
+      test.identical( got, expected );
+    } )( testCase2 );
+
+    /**/
+
+    test.description = 'test tap in chain';
+
+    ( function ( { givSequence, got, expected }  )
+    {
+      function testTaker1( err, value )
+      {
+        var takerId = 'taker1';
+        got.gotSequence.push( { err, value, takerId } );
+        value++;
+        return value;
+      }
+
+      function testTaker2( err, value )
+      {
+        var takerId = 'taker2';
+        got.gotSequence.push( { err, value, takerId } );
+      }
+
+      function testTaker3( err, value )
+      {
+        var takerId = 'taker3';
+        got.gotSequence.push( { err, value, takerId } );
+      }
+
+      var con = wConsequence();
+
+      con.give( givSequence.shift() );
+      con.error( givSequence.shift() );
+      con.give( givSequence.shift() );
+
+      try
+      {
+        con.ifNoErrorThen( testTaker1 );
+        con.ifNoErrorThen( testTaker2 );
+        con.got( testTaker3 );
+
+      }
+      catch( err )
+      {
+        got.throwErr = !! err;
+      }
+      test.identical( got, expected );
+    } )( testCase3 );
+
+    if( Config.debug )
+    {
+      var conDeb1 = wConsequence();
+
+      test.description = 'missed arguments';
+      test.shouldThrowError( function()
+      {
+        conDeb1.ifNoErrorThen();
+      } );
+    }
+
+  };
+
 // --
 // proto
 // --
@@ -1379,7 +1499,8 @@ var Proto =
     // thenReportError: thenReportError,
     // thenClone: thenClone,
     // tap: tap,
-    ifErrorThen: ifErrorThen
+    // ifErrorThen: ifErrorThen,
+    ifNoErrorThen: ifNoErrorThen
   },
 
   name : 'Consequence',
