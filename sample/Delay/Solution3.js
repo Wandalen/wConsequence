62ea8852b08c0e392aa00b3b1b367ee141d3afce
volodymyrmigdal <volodymyr.migdal@gmail.com>
2017-08-03 14:43:13 +0300

    add delay sample

---
+++ sample/Delay/Solution3.js
@@ -0,0 +1,19 @@
+require( 'wTools' );
+require( 'wConsequence' );
+
+/* Solution - using same approach, but now timeOutThen runs our delayed task */
+
+function taskWithDelay( delay, task )
+{
+  var con = new wConsequence();
+  con.give();
+  con.timeOutThen( delay, task );
+  return con;
+}
+
+taskWithDelay( 1000, function()
+{
+   console.log( 'Message with delay' );
+})
+
+console.log( 'Message without delay' ); 
