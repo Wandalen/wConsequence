c0510eca19e04cfa2e09b528efb805e4bc766f17
Wandalen <wandalen.me@gmail.com>
2018-11-05 16:59:28 +0200

    auto

--- proto/dwtools/abase/l9/consequence/Consequence.s
+++ proto/dwtools/abase/l9/consequence/Consequence.s
@@ -23,7 +23,9 @@ chainer :
 if( typeof module !== 'undefined' )
 {
 
+  debugger;
   let _ = require( '../../../Tools.s' );
+  debugger;
 
   _.include( 'wProto' );
   _.include( 'wCopyable' );
@@ -3672,7 +3674,7 @@ _realGlobal_[ Self.name ] = Self;
 
 if( typeof module !== 'undefined' )
 if( _global_.WTOOLS_PRIVATE )
-delete require.cache[ module.id ];
+{ /* delete require.cache[ module.id ]; */ }
 
 if( typeof module !== 'undefined' && module !== null )
 module[ 'exports' ] = Self;
