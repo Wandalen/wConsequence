5a21d1a909f36cca64fcaec197495e941caece2d
Volodymyr Migdal <volodymyr.migdal@gmail.com>
2019-12-11 21:48:54 +0200

    experiment

--- sample/Sample.js
+++ sample/Sample.js
@@ -2,13 +2,15 @@
 if( typeof module !== 'undefined' )
 require( 'wConsequence' );
 var _ = wTools;
-var con = new wConsequence();
+_.Consequence.UnhandledTimeOut = 1;
+var t1 = _.time.now();
+var con = new _.Consequence().error( 'x' )
+con.catch( err => 
+{ 
+  var t2 = _.time.now();
+  _.errAttend( err );
+  console.log( 'Error attended in:', t2 - t1 )
+  return null;
+})
 
-con.got( function( err,data )
-{
 
-  console.log( 'got :',data );
-
-});
-
-con.give( 'some data' );
