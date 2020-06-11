84f53156f94a17115a98cda5be7f705f15f21794
Artem Bohush <art.bohush@gmail.com>
2020-06-11 09:52:56 +0300

    two king of resources, progress

--- sample/TwoKindOfResources.js
+++ sample/TwoKindOfResources.js
@@ -1,7 +1,35 @@
 let _ = require( 'wTools' );
 require( 'wConsequence' );
 
-let con = new _.Consequence();
+// var con = new _.Consequence();
+
+// con.thenGive( ( arg ) => console.log( arg + 1 ) );
+// con.take( 'agr' );
+
+// con.thenGive( ( arg ) => console.log( arg.name ) );
+// con.take({ name : 'user1', age : 20 });
+
+// con.thenGive( ( arg ) => console.log( arg + 1 ) );
+// con.take( 123 );
+
+/* */
+
+// var con = new _.Consequence();
+
+// con.thenGive( ( arg ) => console.log( arg ) );
+// con.take( _.errAttend( 'Error!' ) );
+
+/* */
+var con = new _.Consequence();
+
+con.thenGive( ( arg ) => console.log( arg + 1 ) );
+con.catch( ( arg ) => console.log( arg ) );
+
+con.take( _.errAttend( 'Error!' ) );
+
+/* */
+
+var con = new _.Consequence();
 
 con.then( ( arg ) =>
 {
@@ -23,3 +51,7 @@ con.finally( ( err, arg ) =>
 // console.log( con.argumentsGet() );
 con.error( _.errAttend( 'Error!' ) );
 console.log( con.argumentsGet() );
+
+/* */
+
+
