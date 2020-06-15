c4a67a1ebb3ca9a348bb3883fba8992fa014d2bd
Artem Bohush <art.bohush@gmail.com>
2020-06-15 14:42:18 +0300

    Resource concept done

--- sample/concept/Resource.js
+++ sample/concept/Resource.js
@@ -34,10 +34,13 @@ var con = new _.Consequence();
 con.take( 'my resource1' );
 console.log( con.resourcesGet().length ); // logs: 1
 
+con.then( ( argument ) =>
+{
+  console.log( argument ); // logs: my resource1
+  return 'from then';
+} )
+
 // right after the resource appears in the queue, the callback function that was passed
 // to .thenGive()is invoked with this resource as a parameter
-con.thenGive( ( argument ) => console.log( argument ) ); // logs: my resource1
+con.thenGive( ( argument ) => console.log( argument ) ); // logs: from then
 console.log( con.resourcesGet().length ); // logs: 0
-
-/*  */
-
