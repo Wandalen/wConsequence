13e7d6027adc77bf6c5cbec9d903a2aee8aa569d
Artem Bohush <art.bohush@gmail.com>
2020-06-17 13:01:07 +0300

    BuildingH20 task, some idea very beginning

--- sample/classic/buildingH2O/Solution.js
+++ sample/classic/buildingH2O/Solution.js
@@ -1,5 +1,4 @@
 /* qqq : implement */
-
 let _;
 
 if( typeof module !== 'undefined' )
@@ -8,16 +7,47 @@ if( typeof module !== 'undefined' )
   require( 'wConsequence' );
 }
 
+let hydTotal = 100;
+let oxTotal = 50;
+const molecul = _.Consequence({ capacity : 0 });
+
+run( hydTotal, oxTotal );
+
+//
+
+function run( h, o )
+{
+  for( let i = 0; i < h; i++ )
+  Hydrogen();
+
+  for( let i = 0; i < o; i++ )
+  Oxygen();
+}
+
 //
 
 function Hydrogen()
 {
-
+  return _.time.out( _.intRandom( [ 0, 10000 ] ), () =>
+  {
+    molecul.take( 'h' );
+    molecul.thenGive( ( atom ) =>
+    {
+      console.log( atom );
+    } )
+  } )
 }
 
 //
 
 function Oxygen()
 {
-  
+  return _.time.out( _.intRandom( [ 0, 10000 ] ), () =>
+  {
+    molecul.take( 'o' );
+    molecul.thenGive( ( atom ) =>
+    {
+      console.log( atom );
+    } )
+  } )
 }
