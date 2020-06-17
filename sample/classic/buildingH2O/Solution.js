47990fb6fd34a5c58e6f331037a0eb6140da39f4
Artem Bohush <art.bohush@gmail.com>
2020-06-17 15:02:48 +0300

    BuildingH2O, some progress

--- sample/classic/buildingH2O/Solution.js
+++ sample/classic/buildingH2O/Solution.js
@@ -9,45 +9,53 @@ if( typeof module !== 'undefined' )
 
 let hydTotal = 100;
 let oxTotal = 50;
-const molecul = _.Consequence({ capacity : 0 });
+const formedMolecules = [];
 
-run( hydTotal, oxTotal );
+const waitingHyd = [];
+const waitingOx = [];
+
+let hyd = new _.Consequence();
+let ox = new _.Consequence();
+
+formedMolecules.push( [ Hydrogen(), Hydrogen(), Oxygen() ] );
+
+//
+
+function addHyd()
+{
+  waitingHyd.push( 'h' );
+}
+
+//
+
+function addOx()
+{
+  waitingOx.push( 'o' );
+}
 
 //
 
 function run( h, o )
 {
   for( let i = 0; i < h; i++ )
-  Hydrogen();
+  _.time.out( _.intRandom( [ 0, 10000 ] ), addHyd );
 
   for( let i = 0; i < o; i++ )
-  Oxygen();
+  _.time.out( _.intRandom( [ 0, 10000 ] ), addOx );
 }
 
 //
 
 function Hydrogen()
 {
-  return _.time.out( _.intRandom( [ 0, 10000 ] ), () =>
-  {
-    molecul.take( 'h' );
-    molecul.thenGive( ( atom ) =>
-    {
-      console.log( atom );
-    } )
-  } )
+  hyd.deasync();
+  return hyd.sync();
 }
 
 //
 
 function Oxygen()
 {
-  return _.time.out( _.intRandom( [ 0, 10000 ] ), () =>
-  {
-    molecul.take( 'o' );
-    molecul.thenGive( ( atom ) =>
-    {
-      console.log( atom );
-    } )
-  } )
+  ox.deasync();
+  return ox.sync();
 }
