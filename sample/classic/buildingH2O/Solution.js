65054c69a7ad1cde06952a8fbc77be0f1a930441
Artem Bohush <art.bohush@gmail.com>
2020-06-17 16:39:45 +0300

    BuildingH2O task, implemented

--- sample/classic/buildingH2O/Solution.js
+++ sample/classic/buildingH2O/Solution.js
@@ -7,30 +7,39 @@ if( typeof module !== 'undefined' )
   require( 'wConsequence' );
 }
 
-let hydTotal = 100;
-let oxTotal = 50;
+let hydTotal = 20;
+let oxTotal = 10;
 const formedMolecules = [];
 
-const waitingHyd = [];
-const waitingOx = [];
+const waitingAtoms = { hyd : 0, ox : 0 }
 
 let hyd = new _.Consequence();
 let ox = new _.Consequence();
+let con = new _.Consequence().take( null );
 
-formedMolecules.push( [ Hydrogen(), Hydrogen(), Oxygen() ] );
+run( hydTotal, oxTotal );
+
+let l = hydTotal / 2 < oxTotal ? hydTotal / 2 : oxTotal;
+for( let i = 0; i < l; i++ )
+con.then( () =>
+{
+  formedMolecules.push( [ Hydrogen(), Hydrogen(), Oxygen() ] );
+  console.log( formedMolecules );
+
+  hyd = new _.Consequence();
+  ox = new _.Consequence();
+  return null;
+} );
 
 //
 
-function addHyd()
+function formMolecule()
 {
-  waitingHyd.push( 'h' );
-}
+  hyd.take( 'h' );
+  ox.take( 'o' );
 
-//
-
-function addOx()
-{
-  waitingOx.push( 'o' );
+  waitingAtoms.hyd -= 2;
+  waitingAtoms.ox -= 1;
 }
 
 //
@@ -38,10 +47,30 @@ function addOx()
 function run( h, o )
 {
   for( let i = 0; i < h; i++ )
-  _.time.out( _.intRandom( [ 0, 10000 ] ), addHyd );
+  _.time.out( _.intRandom( [ 5000, 10000 ] ), addHyd );
 
   for( let i = 0; i < o; i++ )
-  _.time.out( _.intRandom( [ 0, 10000 ] ), addOx );
+  _.time.out( _.intRandom( [ 5000, 10000 ] ), addOx );
+}
+
+//
+
+function addHyd()
+{
+  waitingAtoms.hyd += 1;
+
+  if( waitingAtoms.hyd >= 2 && waitingAtoms.ox )
+  formMolecule();
+}
+
+//
+
+function addOx()
+{
+  waitingAtoms.ox += 1;
+
+  if( waitingAtoms.hyd >= 2 )
+  formMolecule();
 }
 
 //
