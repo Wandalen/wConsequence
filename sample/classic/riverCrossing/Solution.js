4d9c4029aab48fd69d568f451f5885ae7a86c0f1
Artem Bohush <art.bohush@gmail.com>
2020-06-18 13:16:46 +0300

    RiverCrossing task, very beginning

--- sample/classic/riverCrossing/Solution.js
+++ sample/classic/riverCrossing/Solution.js
@@ -7,3 +7,56 @@ if( typeof module !== 'undefined' )
   require( 'wConsequence' );
 }
 
+const startTime = _.time.now();
+const con = new _.Consequence();
+const waitingPas = [];
+
+const passagers =
+[
+  { id : 1, fraction : 'employee', delay : 1000 },
+  { id : 2, fraction : 'hacker', delay : 1500 },
+  { id : 3, fraction : 'employee', delay : 2500 },
+  { id : 4, fraction : 'employee', delay : 3500 },
+]
+
+//
+
+function run()
+{
+
+}
+
+//
+
+function status()
+{
+
+}
+
+//
+
+function hackerArrives()
+{
+
+}
+
+//
+
+function employeeArrives()
+{
+
+}
+
+//
+
+function rowBoat()
+{
+
+}
+
+//
+
+function boardBoat()
+{
+
+}
