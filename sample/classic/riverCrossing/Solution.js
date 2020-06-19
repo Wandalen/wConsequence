4bce694f16c74ad8d13e25b26faac4af88380bb4
Artem Bohush <art.bohush@gmail.com>
2020-06-19 19:24:10 +0300

    RiverCrossing task, separated into problem and solution

--- sample/classic/riverCrossing/Solution.js
+++ sample/classic/riverCrossing/Solution.js
@@ -1,10 +1,11 @@
-/* aaa Artem : done. implement */
-let _;
+let _,
+  Problem;
 
 if( typeof module !== 'undefined' )
 {
   _ = require( 'wTools' );
   require( 'wConsequence' );
+  Problem = require( './Problem.js' );
 }
 
 const startTime = _.time.now();
@@ -13,39 +14,8 @@ let waitingPass = [];
 let passOnBoard = [];
 const riverCrossingTime = 10000;
 let crossingRiver = false;
-
-const passengers =
-[
-  { id : 1, fraction : 'employee', arrival : 1000 },
-  { id : 2, fraction : 'hacker', arrival : 1500 },
-  { id : 3, fraction : 'employee', arrival : 2500 },
-  { id : 4, fraction : 'employee', arrival : 3500 },
-
-  { id : 5, fraction : 'hacker', arrival : 5500 },
-  { id : 6, fraction : 'employee', arrival : 6500 },
-  { id : 7, fraction : 'hacker', arrival : 8000 },
-  { id : 8, fraction : 'employee', arrival : 9500 },
-
-  { id : 9, fraction : 'hacker', arrival : 10500 },
-  { id : 10, fraction : 'employee', arrival : 12000 },
-  { id : 11, fraction : 'hacker', arrival : 13500 },
-  { id : 12, fraction : 'hacker', arrival : 15000 },
-
-  { id : 13, fraction : 'hacker', arrival : 16500 },
-  { id : 14, fraction : 'employee', arrival : 17500 },
-  { id : 15, fraction : 'employee', arrival : 18000 },
-  { id : 16, fraction : 'hacker', arrival : 19500 },
-]
-
-run();
-
-//
-
-function run()
-{
-  for( let i = 0; i < passengers.length; i++ )
-  _.time.out( passengers[ i ].arrival, () => passengerArrives( passengers[ i ] ) );
-}
+Problem.passengerArrives = passengerArrives;
+Problem.run();
 
 //
 
@@ -54,6 +24,40 @@ function status()
   const h = passOnBoard.filter( ( p ) => p.fraction === 'hacker' );
   const e = passOnBoard.filter( ( p ) => p.fraction === 'employee' );
   return `${_.time.spent( startTime )}, boatCrossesRiver: ${crossingRiver}, onBoard: h:${h.length} e:${e.length}, waitingPass: ${waitingPass.length}`;
+}
+
+//
+
+function boardBoat( p )
+{
+  passOnBoard.push( p );
+  console.log( `  pass_${p.id} ${p.fraction} on board - ${status()}` );
+}
+
+//
+
+function rowBoat()
+{
+  crossingRiver = true;
+  console.log( `- boat starts to cross the river - ${status()}` );
+  return _.time.out( riverCrossingTime ).then( nextBoat );
+}
+
+//
+
+function passengerArrives( p )
+{
+  console.log( `+ pass_${p.id} ${p.fraction} arrived - ${status()}` );
+
+  if( crossingRiver )
+  {
+    waitingPass.push( p );
+    console.log( `there is no boat in the dock, pass_${p.id} should wait - ${status()}` );
+  }
+  else
+  {
+    p.fraction === 'hacker' ? hackerArrives( p ) : employeeArrives( p );
+  }
 }
 
 //
@@ -112,40 +116,6 @@ function nextBoat()
 
 //
 
-function passengerArrives( p )
-{
-  console.log( `+ pass_${p.id} ${p.fraction} arrived - ${status()}` );
-
-  if( crossingRiver )
-  {
-    waitingPass.push( p );
-    console.log( `there is no boat in the dock, pass_${p.id} should wait - ${status()}` );
-  }
-  else
-  {
-    p.fraction === 'hacker' ? hackerArrives( p ) : employeeArrives( p );
-  }
-}
-
-//
-
-function boardBoat( p )
-{
-  passOnBoard.push( p );
-  console.log( `  pass_${p.id} ${p.fraction} on board - ${status()}` );
-}
-
-//
-
-function rowBoat()
-{
-  crossingRiver = true;
-  console.log( `- boat starts to cross the river - ${status()}` );
-  return _.time.out( riverCrossingTime ).then( nextBoat );
-}
-
-//
-
 function hackerArrives( h )
 {
   const e = passOnBoard.filter( ( p ) => p.fraction === 'employee' );
@@ -183,3 +153,5 @@ function employeeArrives( e )
     waitingPass.push( e );
   }
 }
+
+/* aaa Artem : done. implement */
