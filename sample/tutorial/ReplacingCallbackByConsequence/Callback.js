7fb3a9b8bec9c4885b4c352729358f503790afcf
Artem Bohush <art.bohush@gmail.com>
2020-06-30 10:53:00 +0300

    after pull

--- sample/tutorial/ReplacingCallbackByConsequence/Callback.js
+++ sample/tutorial/ReplacingCallbackByConsequence/Callback.js
@@ -19,10 +19,8 @@ Dns.resolve4( uri, conseuqence );
 conseuqence.thenGive( ( addresses ) => console.log( `Ips of ${uri} are ${JSON.stringify( addresses )}` ) );
 
 /*
-
 Consequence `conseuqence` is replacement of callback. It is temporary container for `addresses`.
 Consequence `conseuqence` will get error if `Dons.resolve4` will fail.
-
 */
 
 /* aaa Artem : done. use it for tutorial */
