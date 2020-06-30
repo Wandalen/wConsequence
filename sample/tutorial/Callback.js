1aacaf7194a2a77da16f3c54c41dbf77fb1489d4
Artem Bohush <art.bohush@gmail.com>
2020-06-30 10:49:43 +0300

    conflict resolved

---
+++ sample/tutorial/Callback.js
@@ -0,0 +1,26 @@
+let _ = require( 'wTools' );
+require( 'wConsequence' );
+let Dns = require( 'dns' );
+
+let uri = 'google.com';
+
+/* with callback */
+
+Dns.resolve4( uri, ( err, addresses ) =>
+{
+  if( err ) console.log( err );
+  console.log( `Ips of ${uri} are ${JSON.stringify( addresses )}` );
+});
+
+/* with consequence */
+
+let conseuqence = new _.Consequence();
+Dns.resolve4( uri, conseuqence );
+conseuqence.thenGive( ( addresses ) => console.log( `Ips of ${uri} are ${JSON.stringify( addresses )}` ) );
+
+/*
+Consequence `conseuqence` is replacement of callback. It is temporary container for `addresses`.
+Consequence `conseuqence` will get error if `Dons.resolve4` will fail.
+*/
+
+/* aaa Artem : done. use it for tutorial */
