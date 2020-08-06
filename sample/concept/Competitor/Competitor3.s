36a892eaf4521c99f6cd0eadfa143c24dff82153
Dmytro <dm.vict.kr@gmail.com>
2020-08-06 09:48:55 +0300

    Fix asset `Competitor3.s`, add taking of resource

--- sample/concept/Competitor/Competitor3.s
+++ sample/concept/Competitor/Competitor3.s
@@ -8,4 +8,10 @@ con.thenGive( ( resource ) => console.log( resource ) );
 // if there is at least one competitor in the queue, the process will not stop, the consequence will be waiting for the resource
 console.log( con.competitorsGet().length ); // logs: 1
 
-// con.take( 'my resource' );
+var startTime = _.time.now();
+_.time.out( 5000 ).then( () =>
+{
+  con.take( 'my resource' );
+  console.log( `Time spented waiting for resource : ${_.time.spent( startTime )}` );
+  return null;
+});
