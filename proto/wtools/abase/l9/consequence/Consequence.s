ec8557628ef1f8ab84562354b560384eda848c69
Dmytro <dm.vict.kr@gmail.com>
2020-11-24 13:57:32 +0200

    Write JSdoc to routine `AndKeep`

--- proto/wtools/abase/l9/consequence/Consequence.s
+++ proto/wtools/abase/l9/consequence/Consequence.s
@@ -2714,8 +2714,69 @@ function AndTake()
 
 //
 
-/* qqq : cover that procedures of AndTake and AndKeep has correct sourcePath */
-/* qqq : jsdoc please */
+/* aaa : cover that procedures of AndTake and AndKeep has correct sourcePath */ /* Dmytro : covered */
+
+/* qqq : jsdoc please */ /* Dmytro : documented */
+
+/**
+ * Static routine AndKeep() copies each resource, which is received by competitors provided in arguments. The competitors
+ * resolve resources. The routine returns resulted Consequence when all passed competitors will receive resource.
+ *
+ * @example
+ * let track = [];
+ * let con1 = new _.Consequence();
+ * let con2 = new _.Consequence();
+ *
+ * let conOwner = _.Consequence.AndKeep( con1, con2 );
+ *
+ * _.time.out( 50, () =>
+ * {
+ *   track.push( 'con1 take' );
+ *   con1.take( 'value1' );
+ * });
+ * _.time.out( 200, () =>
+ * {
+ *   track.push( 'con2 take' );
+ *   con2.take( 'value2' );
+ * });
+ *
+ * con1.tap( ( err, value ) =>
+ * {
+ *   track.push( 'con1 tap' );
+ *   console.log( `con1 competitor executed with value : ${ value } and error : ${ err }` );
+ * });
+ *
+ * con2.tap( ( err, value ) =>
+ * {
+ *   track.push( 'con2 tap' );
+ *   console.log( `con2 competitor executed with value : ${ value } and error : ${ err }` );
+ * });
+ *
+ * conOwner.finallyGive( ( err, val ) =>
+ * {
+ *   console.log( _.toStr( track ) );
+ *   if( err )
+ *   console.log( `Error : ${ err }` );
+ *   else
+ *   console.log( `Value : ${ _.toStr( val ) }` );
+ * });
+ *
+ * // log :
+ * // con1 competitor executed with value : value1 and error : undefined
+ * // con2 competitor executed with value : value2 and error : undefined
+ * // [ 'con1 take', 'con1.tap', 'con2 take', 'con2.tap' ]
+ * // Value : [ 'value1', 'value2', 100 ]
+ *
+ * @param { * } ... arguments - Unlimited number of competitors of any type in arguments.
+ * @returns { Consequence } - Returns new Consequence when all passed competitors will be resolved.
+ * @throws { Error } If routine calls by instance of Consequence.
+ * @static
+ * @function AndKeep
+ * @module Tools/base/Consequence
+ * @namespace wTools.Consequence
+ * @class wConsequence
+ */
+
 function AndKeep()
 {
   _.assert( !_.instanceIs( this ) )
